6e4a28d9af2b4933ead6dbd9a7da6f2e214282e7
borisyelnikoff <b.zawada84@gmail.com>
2021-05-13 10:14:12 +0200

    Integrate ESLint with Prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    // es2021: true,
     node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
+  plugins: ["prettier"],
   rules: {
-    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
     "no-use-before-define": ["error", { functions: false, classes: false }],
+    "linebreak-style": "off",
   },
 };

dbb64dc3bbfac819dcac9ed6fa57209380bc63de
Boris Yelnikoff <b.zawada84@gmail.com>
2021-05-16 09:32:10 +0200

    Build checkCardNumber function.

---
+++ src/checkCardNumber.js
@@ -0,0 +1,19 @@
+import createCardValidator from "./cardValidator.js";
+import { cardTypes } from "./cardsData.js";
+
+export default function checkCardNumber(cardNumber) {
+  if (typeof cardNumber !== "number") {
+    throw new Error("Invalid argument. Number is expected.");
+  }
+
+  let cardVendor = "Invalid";
+  Object.entries(cardTypes).some(([cardName, cardAttr]) => {
+    const cardValidator = createCardValidator(cardAttr);
+    if (cardValidator.isValid(cardNumber)) {
+      cardVendor = cardName.replace(/^[a-z]/, cardName[0].toUpperCase());
+      return true;
+    }
+  });
+
+  return cardVendor;
+}
